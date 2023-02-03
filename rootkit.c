027b8847869401859c027fddb7125cfcffd94e42
William Martens <68499986+loneicewolf@users.noreply.github.com>
2023-02-03 17:51:15 +0100

    Update rootkit.c

--- rootkit.c
+++ rootkit.c
@@ -1,8 +1,8 @@
 /* GitHub*
  * Inspirations
  * https://github.com/loneicewolf/EXEC_LKM
- * * [compact_linux_reverse_shell.c](https://gist.github.com/loneicewolf/8232aad5722e1e7de9d92932b5a01597)
- * * [compact_windows_reverse_shell.c](https://gist.github.com/loneicewolf/03d71d65735d8b2d34b5c60b1232d144)
+ * [compact_linux_reverse_shell.c](https://gist.github.com/loneicewolf/8232aad5722e1e7de9d92932b5a01597)
+ * [compact_windows_reverse_shell.c](https://gist.github.com/loneicewolf/03d71d65735d8b2d34b5c60b1232d144)
  * 
  * Kernel.org*
  * https://www.kernel.org/doc/htmldocs/kernel-api/API-call-usermodehelper.html
